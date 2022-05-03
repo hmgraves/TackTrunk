4a5cd99188a33447f959c3cca780573d9b0a66e6
Hannah Graves <hannahmgraves@gmail.com>
2022-05-03 16:24:24 -0700

    mern infrastructure

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 
 const AuthPage = ({ setUser }) => {
+	const [ currentForm, setCurrentForm ] = useState('login');
+	const handleClick = () => {
+		if (currentForm === 'login') {
+			setCurrentForm('signup')
+		} else {
+			setCurrentForm('login')
+		}
+	};
 	return (
 		<main>
 			<h1>AuthPage</h1>
-			<SignUpForm setUser={setUser}/>
-			<LoginForm setUser={setUser}/>
+			<h3 onClick={handleClick}>
+				{
+					currentForm === 'login'
+					? <button>Sign Up</button>
+					:
+					<button>Log in</button>
+				}
+			</h3>
+			{
+				currentForm === 'login'
+				? <LoginForm setUser={setUser}/>
+				:
+				<SignUpForm setUser={setUser}/>
+			}
 		</main>
 	);
 };
